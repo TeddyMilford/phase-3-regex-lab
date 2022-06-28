df4203b7642460e11c86e9e4556eec760989b758
Teddy Milford <teddy.milford@gmail.com>
2022-06-27 19:27:30 -0600

    commit tree ce2886d2d54972db2837b50d38dff9a45223980a

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,16 @@
 def starts_with_a_vowel?(word)
-
+  word =~ /[aeiou]/
+  word == 1 ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
 end
 
 def words_five_letters_long(text)
-
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
 end
 
 def valid_phone_number?(phone)
-
 end
